67a24139776fa682a3479e4eb1bb4bf7205b40a6
Felix Lee <haradwaith@naver.com>
2025-07-15 13:14:46 +0900

    fix: bugs

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 80;
+const port = 3000;
 
-// 'public' 폴더를 정적 파일 제공을 위한 디렉토리로 설정합니다.
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, (err) => {
+  if (err) console.error(err);
+  else console.log(`server running on port ${port}`);
+});
